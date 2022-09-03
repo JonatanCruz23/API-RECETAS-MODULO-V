5ea48b0a4039ad1f53848837b3b48b0837fce59b
Esau gomez <alfonsoesaugomez@gmail.com>
2022-09-02 23:33:32 -0600

    enviroment vars added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb+srv://adminmoduloiii:soloyo123@moduloiii.gyrzj.mongodb.net/?retryWrites=true&w=majority'), UsuarioModule, AuthModule],
+  imports: [ConfigModule.forRoot(), MongooseModule.forRoot('mongodb+srv://adminmoduloiii:soloyo123@moduloiii.gyrzj.mongodb.net/?retryWrites=true&w=majority'), UsuarioModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
