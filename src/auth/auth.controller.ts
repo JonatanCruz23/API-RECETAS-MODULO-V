c82830779e6f69d211715312c51261a18dbb9b4f
Esau gomez <alfonsoesaugomez@gmail.com>
2022-09-03 14:36:43 -0600

    fix: ading tags to login screen

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiTags, ApiBody } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { LoginUsuarioDto } from 'src/usuario/dto/login-usuario.dto';
 @ApiTags("Authentication")
 @Controller("auth")
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @ApiBody({type:LoginUsuarioDto})
     @UseGuards(LocalAuthGuard)
     @Post('login')
     async login(@Request() req) {
