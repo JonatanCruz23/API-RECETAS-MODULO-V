221c9d0109bfda6267b431f401540ac31878f657
Esau gomez <alfonsoesaugomez@gmail.com>
2022-09-03 02:18:33 -0600

    feat: swagger library installed

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  // Swagger
+  const swaggerConfig = new DocumentBuilder()
+  .setTitle('API Recetas')
+  .setDescription('Request collections')
+  .setVersion('1.0')
+  .addTag('Recetas')
+  .build();
+  
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, swaggerDocument);
+
   await app.listen(port, () => {
     console.log(`Server listen on port ${port}`);
   });
